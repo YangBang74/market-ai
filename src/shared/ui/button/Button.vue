f542c2eacfa4ef842273b0afec797084bb2f106f
Yan <crowbeat.47@gmail.com>
2026-01-17 22:34:01 +0500

    fix: build error

--- src/shared/ui/button/Button.vue
+++ src/shared/ui/button/Button.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, type Component } from 'vue'
+import { computed } from 'vue'
 import type { Props, ButtonVariant, ButtonSize, ButtonRounded } from './model/type'
 
 
